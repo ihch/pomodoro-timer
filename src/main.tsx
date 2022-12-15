7e8604dfe926a29b2b5a264ae39e8a7933ff13de
ihch <nemu0sou@gmail.com>
2022-12-15 18:03:59 +0900

    try: vite-pwa-pluginを入れてみた

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './index.css'
 
@@ -7,4 +8,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+
+registerSW();
